2c09b5886feefe0c0c3b1f806a138f90f3220de3
dlawotn3 <dlawotn3@naver.com>
2024-05-23 11:03:24 +0000

    chore: update sql

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -6,11 +6,48 @@ FLUSH PRIVILEGES;
 
 USE `krampoline`;
 
-DROP TABLE IF EXISTS `sample_data`;
-CREATE TABLE `sample_data` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `detail` varchar(100) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE plogging_record (
+    plogging_record_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id VARCHAR(255) NOT NULL,
+    trash_count INT NOT NULL,
+    total_calories DOUBLE NOT NULL,
+    moving_time INT NOT NULL,
+    moving_distance DOUBLE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO sample_data (`id`,`detail`) VALUES ('1', 'Hello DKOS!');
+CREATE TABLE moving_coordinate (
+    moving_coordinate_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    plogging_record_id BIGINT,
+    lat DOUBLE NOT NULL,
+    lng DOUBLE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_plogging_record
+        FOREIGN KEY (plogging_record_id) 
+        REFERENCES plogging_record(plogging_record_id)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE trash_coordinate (
+    trash_coordinate_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    plogging_record_id BIGINT,
+    lat DOUBLE NOT NULL,
+    lng DOUBLE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_plogging_record
+        FOREIGN KEY (plogging_record_id) 
+        REFERENCES plogging_record(plogging_record_id)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE member (
+    member_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255),
+    platform VARCHAR(255),
+    platform_id VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE (email, platform)
+);
