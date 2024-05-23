741e402d53e600997c893153273bf0e22d969bb3
jisu <dlawotn3@naver.com>
2024-05-23 19:00:24 +0900

    fix: fix code

--- src/main/java/com/goorm/server/controller/ApiController.java
+++ src/main/java/com/goorm/server/controller/ApiController.java
@@ -1,14 +1,18 @@
 package com.goorm.server.controller;
 
 import com.goorm.server.dto.response.Response;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api")
+@RestController
+@RequiredArgsConstructor
 public class ApiController {
 
     @GetMapping("/test")
     public Response<?> login() {
-        return Response.ofSuccess("OK", null);
+        return null;
     }
 }
